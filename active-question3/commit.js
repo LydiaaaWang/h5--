9f312af8192d3b53479332558090689d2e20f965
wangyuedemail@126.com <wangyuedemail@126.com>
2018-09-17 14:19:31 +0800

    留下一个问题页面，删掉重复的两个

--- active-question3/commit.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by chonger on 2018/4/4.
- */
-~function(){
-    var deviceWidth = document.documentElement.clientWidth;
-    if(deviceWidth > 750) deviceWidth = 750;
-    document.documentElement.style.fontSize = deviceWidth / 7.5 + 'px';
-}();
